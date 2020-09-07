4de1bb58614b4ca1d929383520caa213f773a50e
Blitzy29 <yoann.peron.pro@protonmail.com>
2020-09-07 19:29:18 +0200

    Add guess and timestamp in historical data

--- src/data/collect_data.py
+++ src/data/collect_data.py
@@ -188,7 +188,7 @@ def if_not_correct(vocab, row_to_test, input_language, output_language, your_gue
     input("Write it again   ")
 
 
-def add_historical_data(historical_data, vocab, row_to_test, output_language, is_it_correct):
+def add_historical_data(historical_data, vocab, row_to_test, output_language, is_it_correct, your_guess):
     """ Add try to historical data
 
     Parameters
@@ -203,6 +203,8 @@ def add_historical_data(historical_data, vocab, row_to_test, output_language, is
         The output language
     is_it_correct: boolean
         Is the answer correct
+    your_guess: str
+        The guess word
 
     Return
     -----
@@ -216,7 +218,9 @@ def add_historical_data(historical_data, vocab, row_to_test, output_language, is
         'english_word': vocab.loc[row_to_test, 'english'],
         'score_before': vocab.loc[row_to_test, 'score'],
         'language_asked': output_language,
-        'result': is_it_correct
+        'result': is_it_correct,
+        'guess': your_guess,
+        'timestamp': datetime.datetime.now()
     }, ignore_index=True)
 
     return historical_data
@@ -270,7 +274,6 @@ def update_historical_data(historical_data):
 
     """
 
-    historical_data['day'] = date.today().strftime("%Y-%m-%d")
     historical_data_old = pd.read_csv('data/raw/historical_data.csv')
     historical_data_new = pd.concat([historical_data_old, historical_data], axis=0)
     historical_data_new.to_csv('data/raw/historical_data.csv', index=False)
