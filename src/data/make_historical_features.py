c61bb310a2f619440c1f4f5aa25d843ec9a17f74
Blitzy29 <yoann.peron.pro@protonmail.com>
2020-09-26 21:28:28 +0200

    Modifying historical dataset ; Adding lots of features

--- src/data/make_historical_features.py
+++ src/data/make_historical_features.py
@@ -6,40 +6,89 @@ from Levenshtein import distance
 
 def create_historical_features(historical_data):
 
-    historical_data = add_nb_previous_occurrences(historical_data)
+    historical_data = add_nb_past_occurrences(historical_data)
     historical_data = add_last_occurrence(historical_data)
     historical_data = add_first_occurrence(historical_data)
     historical_data = transfer_features_to_next_datapoint(historical_data)
+    historical_data = add_datetime_features(historical_data)
+    historical_data = add_nb_words_same_session(historical_data)
 
     return historical_data
 
 
-def add_nb_previous_occurrences(historical_data):
+def add_nb_past_occurrences(historical_data):
 
-    historical_data['occurrence'] = 1
+    historical_data["occurrence"] = 1
 
-    previous_occurrences = historical_data.groupby(
-        ['id_vocab', 'language_asked', 'id_historical_data']
-    )['occurrence'].sum().groupby(level=[0, 1]).cumsum().reset_index()
-    previous_occurrences.rename(columns={
-        'occurrence': 'previous_occurrences'
-    }, inplace=True)
-    previous_occurrences['previous_occurrences'] -= 1
+    # same language
+    past_occurrences = (
+        historical_data.groupby(["id_vocab", "language_asked", "id_historical_data"])[
+            "occurrence"
+        ]
+        .sum()
+        .groupby(level=[0, 1])
+        .cumsum()
+        .reset_index()
+    )
+    past_occurrences.rename(
+        columns={"occurrence": "past_occurrences_same_language"}, inplace=True
+    )
+    past_occurrences["past_occurrences_same_language"] -= 1
 
     historical_data = pd.merge(
         historical_data,
-        previous_occurrences[['id_historical_data', 'previous_occurrences']],
-        on='id_historical_data'
+        past_occurrences[
+            ["id_historical_data", "past_occurrences_same_language"]
+        ],
+        on="id_historical_data",
     )
 
-    del historical_data['occurrence']
+    historical_data["past_successes_same_language"] = (
+        historical_data["past_occurrences_same_language"]
+        + historical_data["score_before"]
+    ) / 2
+    historical_data["past_fails_same_language"] = (
+        historical_data["past_occurrences_same_language"]
+        - historical_data["score_before"]
+    ) / 2
 
-    historical_data['previous_successes'] = (
-        historical_data['previous_occurrences'] + historical_data['score_before']
+    # any language
+    past_occurrences = (
+        historical_data.groupby(["id_vocab", "id_historical_data"])["occurrence"]
+        .sum()
+        .groupby(level=0)
+        .cumsum()
+        .reset_index()
+    )
+    past_occurrences.rename(
+        columns={"occurrence": "past_occurrences_any_language"}, inplace=True
+    )
+    past_occurrences["past_occurrences_any_language"] -= 1
+
+    historical_data = pd.merge(
+        historical_data,
+        past_occurrences[
+            ["id_historical_data", "past_occurrences_any_language"]
+        ],
+        on="id_historical_data",
+    )
+
+    historical_data["past_successes_any_language"] = (
+        historical_data["past_occurrences_any_language"]
+        + (
+            historical_data["score_before"]
+            + historical_data["score_before_other_language"]
+        )
     ) / 2
-    historical_data['previous_fails'] = (
-        historical_data['previous_occurrences'] - historical_data['score_before']
+    historical_data["past_fails_any_language"] = (
+        historical_data["past_occurrences_any_language"]
+        - (
+            historical_data["score_before"]
+            + historical_data["score_before_other_language"]
+        )
     ) / 2
+
+    del historical_data["occurrence"]
 
     return historical_data
 
@@ -125,7 +174,9 @@ def get_german_article(x, list_german_article=['der', 'die', 'das']):
 
 
 def add_correct_article(historical_data, list_german_article=['der', 'die', 'das']):
+
     historical_data['german_word_article'] = None
+
     historical_data.loc[
         historical_data['language_asked'] == 'german',
         "german_word_article"
@@ -135,6 +186,7 @@ def add_correct_article(historical_data, list_german_article=['der', 'die', 'das
     ].map(get_german_article)
 
     historical_data['guess_article'] = None
+
     historical_data.loc[
         historical_data['language_asked'] == 'german',
         "guess_article"
@@ -143,11 +195,12 @@ def add_correct_article(historical_data, list_german_article=['der', 'die', 'das
         "guess"
     ].map(get_german_article)
 
-    historical_data['correct_article'] = None
+    historical_data['previous_correct_article'] = None
+
     historical_data.loc[
         (historical_data['language_asked'] == 'german')
         & (historical_data['german_word_article'].isin(list_german_article)),
-        'correct_article'
+        'previous_correct_article'
     ] = historical_data['german_word_article'] == historical_data['guess_article']
 
     del historical_data['german_word_article']
@@ -186,14 +239,14 @@ def add_levenshtein_distance_guess_answer(historical_data):
         lambda x: ' '.join(word for word in x.split(' ') if word not in list_english_article)
     )
 
-    historical_data['levenshtein_distance_guess_answer'] = None
+    historical_data['previous_levenshtein_distance_guess_answer'] = None
     historical_data.loc[
         historical_data['language_asked'] == 'german',
-        "levenshtein_distance_guess_answer"
+        "previous_levenshtein_distance_guess_answer"
     ] = historical_data.apply(lambda x: distance(x['german_word_lv'], x['guess_lv']), axis=1)
     historical_data.loc[
         historical_data['language_asked'] == 'english',
-        "levenshtein_distance_guess_answer"
+        "previous_levenshtein_distance_guess_answer"
     ] = historical_data.apply(lambda x: distance(x['english_word_lv'], x['guess_lv']), axis=1)
 
     del historical_data['german_word_lv']
@@ -211,10 +264,10 @@ def add_only_missed_uppercase(historical_data):
         lambda x: any(c.isupper() for c in x)
     )
 
-    historical_data['only_missed_uppercase'] = None
+    historical_data['previous_only_missed_uppercase'] = None
 
     historical_data.loc[
-        historical_data['language_asked'] == 'german', 'only_missed_uppercase'
+        historical_data['language_asked'] == 'german', 'previous_only_missed_uppercase'
     ] = (
             historical_data['german_word_has_uppercase']
             & (historical_data['language_asked'] == 'german')
@@ -233,25 +286,27 @@ def add_previous_results(historical_data):
     ]
     for i_previous_col in previous_results:
         historical_data[f'previous_{i_previous_col}'] = historical_data[i_previous_col]
+    historical_data['previous_score'] = historical_data['score_before']
+    historical_data['previous_score_other_language'] = historical_data['score_before_other_language']
     return historical_data
 
 
 def add_write_it_again_features(historical_data):
 
-    historical_data['write_it_again_not_null'] = ~historical_data['write_it_again'].isna()
+    historical_data['previous_write_it_again_not_null'] = ~historical_data['write_it_again'].isna()
 
     historical_data.loc[
-        historical_data['write_it_again_not_null'], 'write_it_again_german'
+        historical_data['previous_write_it_again_not_null'], 'previous_write_it_again_german'
     ] = historical_data.loc[
-        historical_data['write_it_again_not_null']
+        historical_data['previous_write_it_again_not_null']
     ].apply(
         lambda row: row['write_it_again'].count(row['german_word']), axis=1
     )
 
     historical_data.loc[
-        historical_data['write_it_again_not_null'], 'write_it_again_english'
+        historical_data['previous_write_it_again_not_null'], 'previous_write_it_again_english'
     ] = historical_data.loc[
-        historical_data['write_it_again_not_null']
+        historical_data['previous_write_it_again_not_null']
     ].apply(
         lambda row: row['write_it_again'].count(row['english_word']), axis=1
     )
@@ -260,11 +315,6 @@ def add_write_it_again_features(historical_data):
 
 
 def transfer_features_to_next_datapoint(historical_data):
-    features_to_transfer = [
-        'correct_article', 'levenshtein_distance_guess_answer', 'only_missed_uppercase',
-        'previous_language_asked', 'previous_result', 'previous_question_time',
-        'write_it_again_not_null', 'write_it_again_german', 'write_it_again_english'
-    ]
 
     historical_data = add_correct_article(historical_data)
     historical_data = add_levenshtein_distance_guess_answer(historical_data)
@@ -272,9 +322,18 @@ def transfer_features_to_next_datapoint(historical_data):
     historical_data = add_previous_results(historical_data)
     historical_data = add_write_it_again_features(historical_data)
 
-    prev_occur = historical_data[['id_vocab', 'language_asked', 'previous_occurrences'] + features_to_transfer].copy()
-    prev_occur['new_occurr'] = prev_occur['previous_occurrences'] + 1
-    del prev_occur['previous_occurrences']
+    features_to_transfer = [
+        'days_since_first_occur_same_language', 'days_since_first_occur_any_language',
+        'previous_correct_article', 'previous_levenshtein_distance_guess_answer', 'previous_only_missed_uppercase',
+        'previous_language_asked', 'previous_result', 'previous_question_time',
+        'previous_write_it_again_not_null', 'previous_write_it_again_german', 'previous_write_it_again_english'
+    ]
+
+    prev_occur = historical_data[
+        ['id_vocab', 'language_asked', 'past_occurrences_same_language'] + features_to_transfer
+    ].copy()
+    prev_occur['new_occurr'] = prev_occur['past_occurrences_same_language'] + 1
+    del prev_occur['past_occurrences_same_language']
 
     for i_feature_to_transfer in features_to_transfer:
         del historical_data[i_feature_to_transfer]
@@ -282,11 +341,62 @@ def transfer_features_to_next_datapoint(historical_data):
     historical_data = pd.merge(
         historical_data,
         prev_occur,
-        left_on=['id_vocab', 'language_asked', 'previous_occurrences'],
+        left_on=['id_vocab', 'language_asked', 'past_occurrences_same_language'],
         right_on=['id_vocab', 'language_asked', 'new_occurr'],
         how='left'
     )
 
     del historical_data['new_occurr']
+
+    return historical_data
+
+
+def add_datetime_features(historical_data):
+
+    historical_data["week_number"] = historical_data["datetime"].apply(
+        lambda x: datetime.datetime.strftime(
+            datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f"), "%V"
+        )
+    )
+
+    historical_data["day_week"] = historical_data["datetime"].apply(
+        lambda x: datetime.datetime.strftime(
+            datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f"), "%u"
+        )
+    )
+
+    historical_data["hour"] = historical_data["datetime"].apply(
+        lambda x: datetime.datetime.strftime(
+            datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f"), "%H"
+        )
+    )
+
+    return historical_data
+
+
+def add_nb_words_same_session(historical_data):
+
+    historical_data.sort_values("datetime", inplace=True)
+    historical_data["occurrence"] = 1
+
+    nb_words_session = (
+        historical_data.groupby(["id_session", "id_historical_data"])["occurrence"]
+            .sum()
+            .groupby(level=0)
+            .cumsum()
+            .reset_index()
+    )
+
+    nb_words_session.rename(columns={"occurrence": "nb_words_session"}, inplace=True)
+
+    historical_data = pd.merge(
+        historical_data,
+        nb_words_session[
+            ["id_historical_data", "nb_words_session"]
+        ],
+        on="id_historical_data",
+    )
+
+    del historical_data["occurrence"]
 
     return historical_data
